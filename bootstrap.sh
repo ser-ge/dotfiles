0cf0205fea0329dc0a62fad42748e402d00887e7
Serge Mosesov <smosesov@gmail.com>
2024-06-07 19:05:33 +0100

    feat: stow structure

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,45 +1,40 @@
 #!/usr/bin/bash
 sudo apt-get update
 
-sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev  libsqlite3-dev  wget  curl fzf  llvm  libncurses5-dev  libncursesw5-dev  xz-utils  tk-dev  libffi-dev  liblzma-dev  python-openssl  git  python3-pip  python3-dev  python3-dev  neovim  yarn  neovim  python3-neovim  postgresql  postgresql-contrib  libpq-dev  mosh  redis-server  libc6  libglapi-mesa  libxdamage1  libxfixes3  libxcb-glx0  libxcb-dri2-0  libxcb-dri3-0  libxcb-present0  libxcb-sync1  libxshmfence1  libxxf86vm1  -y -qq --no-install-recommends
+sudo apt-get install make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev  libsqlite3-dev  wget  curl fzf  llvm  libncurses5-dev  libncursesw5-dev  xz-utils  tk-dev  libffi-dev  liblzma-dev  python-openssl  git  python3-pip  python3-dev  python3-dev  neovim  yarn  neovim  python3-neovim  postgresql  postgresql-contrib  libpq-dev  mosh  redis-server  libc6  libglapi-mesa  libxdamage1  libxfixes3  libxcb-glx0  libxcb-dri2-0  libxcb-dri3-0  libxcb-present0  libxcb-sync1  libxshmfence1  libxxf86vm1  stow -y -qq --no-install-recommends
 
-if [ ! -d "${HOME}/.zsh" ]; then
-  sudo apt install -y zsh
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-  echo " ==> Installing zsh plugins"
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+if [ ! -d "${HOME}/.config/fish" ]; then
+  sudo apt install -y fish
+  sudo apt-add-repository ppa:fish-shell/release-3
+  sudo apt update
+  sudo apt install fish
 fi
 
-if [ ! -d "${HOME}/Dropbox" ]; then
-    pushd "${HOME}" && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
-    popd
-fi
 
 if [ ! -d "${HOME}/.fzf" ]; then
   echo " ==> Installing fzf"
   git clone https://github.com/junegunn/fzf "${HOME}/.fzf"
   pushd "${HOME}/.fzf"
   git remote set-url origin git@github.com:junegunn/fzf.git
-  ${HOME}/.fzf/install --bin --64 --no-bash --no-zsh --no-fish
+  ${HOME}/.fzf/install --bin --64
   popd
 fi
 
-echo "==> Setting shell to zsh..."
-chsh -s /usr/bin/zsh
+echo "==> Setting shell to fish..."
+echo $(which fish) | sudo tee -a /etc/shells
+chsh -s $(which fish)
 
 echo "==> Creating dev directories"
-mkdir -p /root/code
+mkdir -p ~/setup/
 
-if [ ! -d /root/code/dotfiles ]; then
+if [ ! -d ~/dotfiles ]; then
   echo "==> Setting up dotfiles"
   # the reason we dont't copy the files individually is, to easily push changes
   # if needed
-  cd "/root/code"
-  git clone --recursive https://github.com/serg-e/dotfiles.git
+  cd "~/setup"
+  git clone --recursive https://github.com/ser-ge/dotfiles.git
 
-  cd "/root/code/dotfiles"
+  cd "~/setup/dotfiles"
   git remote set-url origin git@github.com:serg-e/dotfiles.git
 
   ln -sfn $(pwd)/.config "${HOME}/.config"
