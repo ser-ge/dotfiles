1a9c3f9cea278b0dd35d0e26ad6d4fa1552ba776
Serge Mosesov <smosesov@gmail.com>
2024-06-11 20:29:53 +0100

    fix: clean up

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-export LANGUAGE="en_US.UTF-8"
-export LC_ALL="en_US.UTF-8"
-export LANG="en_US.UTF-8"
-export LC_TYPE="en_US.UTF-8"
 
-dotfile_packages=(nvim tmux tms fish ptpython pudb starship)
+dotfile_packages=(nvim tmux tms fish ptpython pudb starship bash tmate)
 # Directory where stow packages are located
 stow_dir="$HOME/dotfiles"
 
@@ -14,43 +10,42 @@ target_dir="$HOME"
 # Backup suffix for existing files
 backup_suffix=".before_stow"
 
-[ -e .backups.log ] &&  rm "$stow_dir/.backups.log"
+[ -e .backups.log ] && rm "$stow_dir/.backups.log"
 # Define the array of dotfile packages
 # Get a list of folders in the stow_dir that are tracked by git
 
 # Function to handle existing files and directories before stowing
 backup_conflicting_files() {
-  local package="$1"
-  local conflicts
+    local package="$1"
+    local conflicts
 
-  # Loop through the conflicts and backup each one
-  for file in $(find $package -type f); do
-    local reducedFilePath="${file#*/}"
-    local target_file="$HOME/$reducedFilePath"
-    echo $target_file
-    local backup_file="$target_file$backup_suffix"
+    # Loop through the conflicts and backup each one
+    for file in $(find $package -type f); do
+        local reducedFilePath="${file#*/}"
+        local target_file="$HOME/$reducedFilePath"
+        echo $target_file
+        local backup_file="$target_file$backup_suffix"
 
-    echo "Backing up existing file: $target_file to $backup_file"
-    if [ -e "$backup_file" ]; then
-      echo "Backup file already exists: $backup_file"
-    else
-      # Backup the file or directory
-      mv -- "$target_file" "$backup_file"
-      echo "$backup_file" >> "$stow_dir/.backups.log"
-    fi
-  done
+        echo "Backing up existing file: $target_file to $backup_file"
+        if [ -e "$backup_file" ]; then
+            echo "Backup file already exists: $backup_file"
+        else
+            # Backup the file or directory
+            mv -- "$target_file" "$backup_file"
+            echo "$backup_file" >>"$stow_dir/.backups.log"
+        fi
+    done
 }
 
 # Main loop to stow each package
 for package in "${dotfile_packages[@]}"; do
-  echo "Processing package: $package"
+    echo "Processing package: $package"
 
-  # Backup any conflicting files before stowing
-  backup_conflicting_files "$package"
+    # Backup any conflicting files before stowing
+    backup_conflicting_files "$package"
 
-  # Stow the package
-  stow --target="$target_dir" --dir="$stow_dir" "$package"
+    # Stow the package
+    stow --target="$target_dir" --dir="$stow_dir" "$package"
 done
 
 echo "All packages have been successfully stowed."
-
--- uninstall_dotfiles.sh
+++ uninstall_dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the array of dotfile packages
-dotfile_packages=(nvim tmux tms fish ptpython pudb starship)
+dotfile_packages=(nvim tmux tms fish ptpython pudb starship bash tmate)
 
 # Directory where stow packages are located
 stow_dir="$HOME/projects/dotfiles"
@@ -13,35 +13,33 @@ backup_suffix=".before_stow"
 
 # Function to restore the original files from backup
 restore_original_files() {
-  local package="$1"
+    local package="$1"
 
-  if [ -e "$stow_dir/.backups.log" ]; then
-      while IFS= read -r backup_file; do
-        local original_file="${backup_file%$backup_suffix}"
-        echo "Restoring original file: $original_file from $backup_file"
+    if [ -e "$stow_dir/.backups.log" ]; then
+        while IFS= read -r backup_file; do
+            local original_file="${backup_file%$backup_suffix}"
+            echo "Restoring original file: $original_file from $backup_file"
 
-        if [ -e "$original_file" ]; then
-          echo "Original file already exists: $original_file.  skipping."
-        else
-          mv -- "$backup_file" "$original_file"
-        fi
-      done < "$stow_dir/.backups.log"
-   fi
+            if [ -e "$original_file" ]; then
+                echo "Original file already exists: $original_file.  skipping."
+            else
+                mv -- "$backup_file" "$original_file"
+            fi
+        done <"$stow_dir/.backups.log"
+    fi
 }
 # Function to unstow the packages
 unstow_packages() {
-  # Unstow each package
-  for package in "${dotfile_packages[@]}"; do
-    echo "Unstowing package: $package"
-    stow --delete --target="$target_dir" --dir="$stow_dir" "$package"
-  done
+    # Unstow each package
+    for package in "${dotfile_packages[@]}"; do
+        echo "Unstowing package: $package"
+        stow --delete --target="$target_dir" --dir="$stow_dir" "$package"
+    done
 
-  # Restore original files
-  for package in "${dotfile_packages[@]}"; do
-    restore_original_files "$package"
-  done
+    # Restore original files
+    for package in "${dotfile_packages[@]}"; do
+        restore_original_files "$package"
+    done
 }
 
 unstow_packages
-echo "All packages have been successfully unstowed and original files restored."
-
