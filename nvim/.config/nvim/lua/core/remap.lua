0e2ea0a731be466b6d137f7bdb32a7534bdfaa2b
Your Name <smosesov@gmail.com>
2026-02-26 21:22:16 +0000

    fix: tmux pane navigation and update which-key to v3 API
    - Remove C-j/C-k quickfix bindings conflicting with vim-tmux-navigator
    - Update whichkey.lua from v1 to v3 API (window->win, remove deprecated opts)

--- nvim/.config/nvim/lua/core/remap.lua
+++ nvim/.config/nvim/lua/core/remap.lua
@@ -26,8 +26,8 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")  -- conflicts with vim-tmux-navigator
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")  -- use <leader>n / <leader>b instead
 
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
